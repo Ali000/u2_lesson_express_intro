825b68c8c74f6d1ac65ae1766dfb55fe70d1f88f
Ali Albanna <ali.al-banna@outlook.com>
2024-01-28 15:13:53 +0300

    ejs install, path require, engine set, path join

--- intro-express/server.js
+++ intro-express/server.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Hello Express!</h1>");
 });
 
 app.get('/user')
 
+app.get("/home", (req, res) => {
+  res.render("home");
+});
+
+app.post("/");
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
-
