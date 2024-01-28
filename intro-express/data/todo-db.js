20a6b27f4b948d3c2d3caf3910389438c5228186
Ali Albanna <ali.al-banna@outlook.com>
2024-01-28 16:18:47 +0300

    ejs render html, todo db connect

---
+++ intro-express/data/todo-db.js
@@ -0,0 +1,15 @@
+// data/todo-db.js
+
+const todos = [
+  {todo: 'Feed Dogs', done: true},
+  {todo: 'Learn Express', done: false},
+  {todo: 'Buy Milk', done: false}
+];
+
+const getAll = () => {
+  return todos;
+};
+
+module.exports = {
+  getAll
+};
--- intro-express/server.js
+++ intro-express/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const path = require("path");
+const todoDb = require("./data/todo-db");
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+app.get("/todos", (req, res) => {
+  const todos = todoDb.getAll();
+
+  res.render("todos/index", {todos});
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello Express!</h1>");
